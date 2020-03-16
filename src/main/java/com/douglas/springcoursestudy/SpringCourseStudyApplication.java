1cc76f9501090fecec8c3b7d27623bdab4ddf556
dldouglaslopes <dldouglaslopes@gmail.com>
2020-03-16 14:07:13 -0300

    Initial commit

---
+++ src/main/java/com/douglas/springcoursestudy/SpringCourseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.springcoursestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseStudyApplication.class, args);
+	}
+
+}
