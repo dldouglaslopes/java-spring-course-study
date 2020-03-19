393addf850d8188f99169daa40e1d45595d33825
dldouglaslopes <dldouglaslopes@gmail.com>
2020-03-19 13:15:12 -0300

    Create user repository tests

--- src/main/java/com/douglas/springcoursestudy/domain/User.java
+++ src/main/java/com/douglas/springcoursestudy/domain/User.java
@@ -20,10 +20,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Entity(name = "user")
 public class User implements Serializable {
 	
--- src/main/java/com/douglas/springcoursestudy/repository/RequestRepository.java
+++ src/main/java/com/douglas/springcoursestudy/repository/RequestRepository.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.douglas.springcoursestudy.domain.Request;
 import com.douglas.springcoursestudy.enums.RequestState;
 
+@Repository
 public interface RequestRepository extends JpaRepository<Request, Long>{
 
 	public List<Request> findAllByOwnerId(Long id);
 	
-	@Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
 	public Request updateStatus(Long id, RequestState state);
 }
