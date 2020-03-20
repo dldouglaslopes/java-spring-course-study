44e66a659bfdbf9b0c74aa528edec085c13d392d
dldouglaslopes <dldouglaslopes@gmail.com>
2020-03-20 15:26:43 -0300

    Create request stage service

---
+++ src/main/java/com/douglas/springcoursestudy/service/RequestStageService.java
@@ -0,0 +1,47 @@
+package com.douglas.springcoursestudy.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douglas.springcoursestudy.domain.RequestStage;
+import com.douglas.springcoursestudy.enums.RequestState;
+import com.douglas.springcoursestudy.repository.RequestRepository;
+import com.douglas.springcoursestudy.repository.RequestStageRepository;
+
+@Service
+public class RequestStageService {
+	
+	@Autowired
+	private RequestStageRepository requestStageRepository;
+	@Autowired
+	private RequestRepository requestRepository;
+	
+	public RequestStage save(RequestStage stage) {
+		stage.setRealizationDate(new Date());
+		
+		RequestStage createdStage = requestStageRepository.save(stage);
+		
+		Long requestId = stage.getRequest().getId();
+		RequestState state = stage.getState();	
+		
+		requestRepository.updateStatus(requestId, state);
+		
+		return createdStage;
+	}
+	
+	public RequestStage getById(Long id) {
+		Optional<RequestStage> result = requestStageRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<RequestStage> listAllByRequestId(Long requestId) {
+		List<RequestStage> stages = requestStageRepository.findAllByRequestId(requestId);
+		
+		return stages;
+	}
+	
+}
