88e318f0fb51dc60a535c87472a9b7d53262a1ac
dldouglaslopes <dldouglaslopes@gmail.com>
2020-03-25 14:57:31 -0300

    Config provided scope for tomcat

--- src/main/java/com/douglas/springcoursestudy/SpringCourseStudyApplication.java
+++ src/main/java/com/douglas/springcoursestudy/SpringCourseStudyApplication.java
@@ -2,10 +2,17 @@ package com.douglas.springcoursestudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCourseStudyApplication {
+public class SpringCourseStudyApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringCourseStudyApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseStudyApplication.class, args);
 	}
