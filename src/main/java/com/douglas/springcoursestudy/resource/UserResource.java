43af2fa77f77829913e7eea0ec431378f9696563
dldouglaslopes <dldouglaslopes@gmail.com>
2020-03-28 12:49:23 -0300

    Create handle bad credentials and handle access denied exceptions

--- src/main/java/com/douglas/springcoursestudy/resource/UserResource.java
+++ src/main/java/com/douglas/springcoursestudy/resource/UserResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -46,6 +47,7 @@ public class UserResource {
 	@Autowired
 	private JwtManager jwtManager;
 	
+	@Secured({ "ROLE_ADMINISTRATOR" })
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody @Valid UserSaveDTO userDTO) {
 		User createdUser = userService.save(userDTO.toUser());
@@ -103,6 +105,7 @@ public class UserResource {
 		return ResponseEntity.ok(pm);
 	}
 	
+	@Secured({ "ROLE_ADMINISTRATOR" })
 	@PatchMapping("/role/{id}")
 	public ResponseEntity<?> updateRole(@PathVariable(name = "id") Long id,
 										@RequestBody @Valid UserUpdateRoleDTO userUpdateRoleDTO) {
--- src/main/java/com/douglas/springcoursestudy/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/douglas/springcoursestudy/resource/exception/ResourceExceptionHandler.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,6 +24,22 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
 									ex.getMessage(), 
 									new Date());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex) {
+		ApiError error = new ApiError(HttpStatus.UNAUTHORIZED.value(), 
+									ex.getMessage(), 
+									new Date());
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+	}
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<ApiError> handleAccessDeniedException(BadCredentialsException ex) {
+		ApiError error = new ApiError(HttpStatus.FORBIDDEN.value(), 
+									ex.getMessage(), 
+									new Date());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
 	}
 	
 	@Override
